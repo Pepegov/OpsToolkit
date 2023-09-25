95d99e2e932d5d7c92b2ef18436a51c4986b8bd3
pepegov <andelismore@gmail.com>
2023-09-26 02:46:27 +0300

    add minio

--- src/postgres/make-dirs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo mkdir /opt/DockerPostgres/
-sudo mkdir /opt/DockerPostgres/data
-sudo mkdir /opt/DockerPostgres/pgadmin
--- src/rabbitmq/make-dirs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo mkdir /opt/DockerRabbitmq
