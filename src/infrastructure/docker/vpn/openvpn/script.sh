254895bf63ea8faf7d01aa045e59b37ee4fd8fdc
Pepegov <andelismore@gmail.com>
2024-11-30 20:06:13 +0300

    Fix openvpn start script

--- src/infrastructure/docker/vpn/openvpn/script.sh
+++ src/infrastructure/docker/vpn/openvpn/script.sh
@@ -14,7 +14,7 @@ then
 
 elif [ $1 == "start" ]
 then
-        docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
+        docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN --device /dev/net/tun kylemanna/openvpn
 
 elif [ $1 == "add_client" ]
 then
